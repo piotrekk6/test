35286f2113aae1df47d6ff94d8220d808c8d62a9
Piotr Król <piotr.krol@billennium.pl>
2018-01-12 10:20:27 +0100

    zad1 done

---
+++ src/main/java/com/krol/webapp/Info.java
@@ -0,0 +1,52 @@
+package com.krol.webapp;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class Info {
+
+    @Value("${os.name}")
+    private String osName;
+
+    @Value("${JAVA_HOME}")
+    private String javaHome;
+
+    @Value("${spring.profiles.active}")
+    private String activeProfile;
+
+    @Value("${spring.version}")
+    private String springVersion;
+
+    @Value("${version}")
+    private String projectVersion;
+
+    @Value("${basedir}")
+    private String basedir;
+
+    @Value("${name}")
+    private String name;
+
+    @Value("${myFilteredProperty}")
+    private String myFilteredProperty;
+
+
+
+
+    @RequestMapping("/test")
+    public String test(Model model) {
+
+        model.addAttribute("profile",activeProfile);
+        model.addAttribute("osname", osName);
+        model.addAttribute("JAVA_HOME", javaHome);
+        model.addAttribute("springVersion", springVersion);
+        model.addAttribute("projectVersion", projectVersion);
+        model.addAttribute("basdir", basedir);
+        model.addAttribute("name", name);
+        model.addAttribute("myFilteredProperty", myFilteredProperty);
+
+        return "index";
+    }
+}
---
+++ src/main/webapp/WEB-INF/index.jsp
@@ -0,0 +1,14 @@
+<html>
+<body>
+
+name: ${name}<br>
+profile: ${profile} <br>
+os.name: ${osname} <br>
+JAVA_HOME: ${JAVA_HOME}<br>
+spring.version: ${springVersion}<br>
+version: ${projectVersion}<br>
+basedir: ${basdir}<br>
+myFilteredProperty: ${myFilteredProperty}<br>
+
+</body>
+</html>
