4218e532c33e793c6bff000876c0633ff1c550e5
Piotr Król <piotr.krol@billennium.pl>
2018-01-15 13:36:34 +0100

    3-a works

--- src/main/java/com/krol/webapp/Info.java
+++ src/main/java/com/krol/webapp/Info.java
@@ -4,8 +4,7 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
-//import pl.external.MyClass;
-
+import pl.external.MyClass;
 
 
 @Controller
@@ -50,10 +49,8 @@ public class Info {
         model.addAttribute("basdir", basedir);
         model.addAttribute("name", name);
         model.addAttribute("myFilteredProperty", myFilteredProperty);
-        //model.addAttribute("methodOutput", MyClass.myStaticMethodOutput());
-
-
-
+        model.addAttribute("methodOutput", MyClass.myStaticMethodOutput());
+        
         return "index";
     }
 }
